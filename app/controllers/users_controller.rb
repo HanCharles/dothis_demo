cdcfe5bdccf471d81a59964fedd2a4122da91e5d
HanCharles <charleshan1985@gmail.com>
2015-04-27 21:50:48 +0900

    modify User page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@posts = @user.posts.all.order("created_at DESC")
-		# render layout: 'fancybox'
+		@posts_story = @user.posts.all.where(is_sale: false).order("created_at DESC")
+		@posts_sale = @user.posts.all.where(is_sale: true).order("created_at DESC")
 	end
 end
