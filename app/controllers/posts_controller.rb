6909119f64f580bfe1cdae8421d26ecff1ec5806
HanCharles <charleshan1985@gmail.com>
2015-05-04 14:31:00 +0900

    no need login for category_select

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote]
-	before_action :authenticate_user!, except: [:index, :show, :search]
+	before_action :authenticate_user!, except: [:index, :show, :search, :category_select]
 
 	def search
 		if params[:search]
